b31abdc8888186acb3198359f852527510fc3bfd
djkloop <1044032256@qq.com>
2025-01-09 19:26:40 +0800

    Implement main menu functionality in demo_bill.rs with input handling and options for adding and viewing bills

--- rs_practise/src/demo_bill.rs
+++ rs_practise/src/demo_bill.rs
@@ -1,6 +1,51 @@
-fn get_input
+use std::io;
 
+fn get_input() -> Option<String> {
+    let mut input = String::new();
+    while io::stdin().read_line(&mut input).is_err() {
+        println!("Please enter your data again.");
+    }
+    let input = input.trim().to_owned();
+    if &input == "" {
+        None
+    } else {
+        Some(input)
+    }
+}
+
+enum MainMenu {
+    AddBill,
+    ViewBill,
+}
+
+impl MainMenu {
+    fn from_str(input: &str) -> Option<Self> {
+        match input {
+            "1" => Some(Self::AddBill),
+            "2" => Some(Self::ViewBill),
+            _ => None,
+        }
+    }
+
+    fn show() {
+        println!("");
+        println!("Welcome to Bill Manager");
+        println!("1. Add bill");
+        println!("2. View bill");
+        println!("");
+        println!("Enter Selection: ");
+    }
+}
 
 pub fn demo_bill() {
-    println!("Hello, world!");
+
+    loop {
+        MainMenu::show();
+        let input = get_input().expect("Failed to read line");
+        match MainMenu::from_str(input.as_str()) {
+            Some(MainMenu::AddBill) => (),
+            Some(MainMenu::ViewBill) => (),
+            None => return
+        }
+    }
 }
